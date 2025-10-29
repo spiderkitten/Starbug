334062aafe9199d6c084ecbc153a5face02431b3
Astrogirl <starfleetbrat@gmail.com>
2025-10-29 15:57:20 +1100

    .

--- .eleventy.js
+++ .eleventy.js
@@ -15,8 +15,7 @@ module.exports = function (eleventyConfig) {
       eleventyConfig.addPassthroughCopy("local/robots.txt");
       eleventyConfig.addPassthroughCopy("local/beehappy/style.css");
 
-
-
+eleventyConfig.addFilter("limit", function (arr, limit) {  return arr.slice(0, limit);});
 
       // blogDate pairs with function below
       eleventyConfig.addFilter('blogDate', blogDate) 
